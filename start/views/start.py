303bc6d3c2a5cad7834cfd3d1dd54ca87595fee4
hlopik <evg@it-solution.ru>
2023-07-03 16:42:52 +0300

    readme.md

--- start/views/start.py
+++ start/views/start.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
 
+from django.conf import settings
 
 @main_auth(on_start=True, set_cookie=True)
 def start(request):
+    app_settings = settings.APP_SETTINGS
     return render(request, 'start_page.html', locals())
