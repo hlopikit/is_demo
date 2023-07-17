5d097e73e55a858263831d046eab8ccecc4cb8fc
hlopik <evg@it-solution.ru>
2023-07-17 19:33:41 +0300

    fix

--- urls.py
+++ urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('company_on_map/', include('company_on_map.urls')),
     path('robot/', include('robot_currency.urls', 'bitrix_robot_currency')),
     path('employeegrid/', include('employeegrid.urls')),
-    path('product_list_in_excel/', include('product_list_excel.urls')),
+    #path('product_list_in_excel/', include('product_list_excel.urls')),
     path('allcompbizproc/', include('allcompbizproc.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
