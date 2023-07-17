768cd4e36ad3b47c0f56b5fa014302d22e7390e1
Iliya <kiv.itsolutions@gmail.com>
2023-07-17 14:18:24 +0300

    added new app for launching BP for companies

---
+++ allcompbizproc/forms/selectBP.py
@@ -0,0 +1,15 @@
+from django import forms
+from allcompbizproc.models import BizprocModel
+
+
+class BPForm(forms.ModelForm):
+    class Meta:
+        model = BizprocModel
+        fields = []
+    BP = forms.ModelChoiceField(
+        queryset=BizprocModel.objects.all(),
+        to_field_name='process_id',
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='БП'
+    )
---
+++ allcompbizproc/views/runbizproc.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from allcompbizproc.forms.selectBP import BPForm
+from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
+
+from allcompbizproc.models.bizprocmodel import BizprocModel
+
+
+@main_auth(on_cookies=True)
+def run_bizproc(request):
+    but = request.bitrix_user_token
+    companies_id = but.call_list_method('crm.company.list', {'select': ['ID']})
+    BizprocModel.find_all_bizprocs(but)
+    form = BPForm()
+    if request.method == 'POST':
+        form = BPForm(request.POST)
+        if form.is_valid():
+            cur_bp = form.cleaned_data['BP']
+        for company in companies_id:
+            cur_bp.run_cur_bizproc(but, company['ID'])
+    return render(request, 'allcompbizproc.html', context={'form': form})
