8ba2430a1db237278813c8126fe02dce4e5dc6ca
vvp_itsolution <vvp@it-solution.ru>
2023-07-20 16:03:41 +0300

    Fix a bug with moving to another application by pressing a button.

--- usermanager/urls.py
+++ usermanager/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views.search_manager import search_manager
 
 urlpatterns = [
-    path('searchmanager/', search_manager, name='register_call'),
+    path('searchmanager/', search_manager, name='search_manager'),
 ]
