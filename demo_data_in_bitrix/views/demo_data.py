6d4e34ae84f08d6858da757b1cffbf57074d128f
Vlad Manylov <mvn@it-solution.ru>
2023-07-20 18:26:22 +0300

    change project "demodata"

--- demo_data_in_bitrix/views/demo_data.py
+++ demo_data_in_bitrix/views/demo_data.py
@@ -27,7 +27,23 @@ def excel(request):
                 but = request.bitrix_user_token
                 data = excel_to_dict(filename, sheet_name)
                 but.call_api_method("crm.item.batchImport", {"entityTypeId": str(OBJECT_CRM[sheet_name]),
-                                                             "data": data})
+                                                             "data": data[0]})
+                if sheet_name == "Компании":
+                    companies = but.call_list_method('crm.company.list', {"SELECT": {"ORIGIN_ID", "ID"}})
+                    for company in companies:
+                        ORIGIN_ID = int(float(company['ORIGIN_ID']))
+                        if ORIGIN_ID in list(data[1].keys()):
+                            data[1][ORIGIN_ID] = company['ID']
+                index = 1
+                for row in data[2]:
+                    but.call_api_method("crm.address.add", {"fields": {
+                        "TYPE_ID": "1",
+                        "ENTITY_TYPE_ID": "4",
+                        "ENTITY_ID": data[1][index],
+                        "CITY": row["CITY"],
+                        "ADDRESS_1": row["ADDRESS_1"],
+                    }})
+                    index += 1
             result = "Сущности успешно добавлены!"
         except Exception as Ex:
             result = "Неверная ссылка"
--- demo_data_in_bitrix/views/utils.py
+++ demo_data_in_bitrix/views/utils.py
@@ -13,15 +13,27 @@ def excel_to_dict(file_path, sheet_name):
     sheet = workbook[sheet_name]
 
     data = list()
+    address_company = list()
+    list_company = list()
+    origin_dict = dict()
 
     headers = [cell.value for cell in sheet[1]]
+    origin_id = 1
     for row in sheet.iter_rows(min_row=2, values_only=True):
         row_data = dict(zip(headers, row))
-        if sheet_name == "Контакты" or sheet_name == "Лиды":
+        if sheet_name == "Контакты":
             row_data["PHONE"] = [{"VALUE": str(row_data["PHONE"]), "VALUE_TYPE": "WORK"}]
+        elif sheet_name == "Компании":
+            address_company.append({"ADDRESS_1": str(row_data["ADDRESS"]), "CITY": str(row_data["ADDRESS_CITY"])})
+            origin_dict[origin_id] = None
         data.append(row_data)
+        origin_id += 1
 
-    return data
+    list_company.append(data)
+    list_company.append(origin_dict)
+    list_company.append(address_company)
+
+    return list_company
 
 
 def get_sheet_names(file_path):
