2d12623a7d99b1256dd7da0abe2291ab748e37fc
hlopik <evg@it-solution.ru>
2023-07-27 15:39:37 +0300

    jfewekfjkfjew skfjweifjiwejfiwe wiejfiwejfjiwfuwejfiwefjiwejfiwejf

--- sample_tg_bot/views/send_message.py
+++ sample_tg_bot/views/send_message.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from telegram import Bot
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+
 from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
-import asyncio
+
+from integration_utils.vendors.telegram import Bot
+
 
 @main_auth(on_cookies=True)
 def send_message(request):
@@ -12,9 +13,7 @@ def send_message(request):
         message_text = request.POST.get('message_text')
         # print(bot_token, chat_id, message_text)
         bot = Bot(token=bot_token)
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        async_result = loop.run_until_complete(bot.send_message(text=message_text, chat_id=chat_id))
-        loop.close()
+        bot.send_message(text=message_text, chat_id=chat_id)
+
 
     return render(request, 'msg_input.html')
