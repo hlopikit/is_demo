aebc64ccac91fff4ead481c0cd3ce91b820efc8b
Vlad Manylov <mvn@it-solution.ru>
2023-07-31 11:33:54 +0300

    add change

--- demo_data_in_bitrix/utils/utils.py
+++ demo_data_in_bitrix/utils/utils.py
@@ -10,6 +10,7 @@ OBJECT_CRM = {"Лиды": 1,
               "Коммерческие предложения": 7,
               "Новые счета": 31}
 
+
 def add_origin_prefix(data, prefix):
     """
     В файле ексель(гугл) док мы делаем связку менжду страницами по полю ORIGIN_ID, т.к при импорте в Б24 мы получим новые ID сущностей.
@@ -26,6 +27,7 @@ def add_origin_prefix(data, prefix):
             d['ORIGIN_ID'] = "{}_{}".format(prefix, d.get('ORIGIN_ID'))
     return data
 
+
 def make_links_from_origin(data, excel_field, b24_field, source_map, prefix):
     # Принимает список из листов экселя
     # заменяет excel_field=COMPANY_ORIGIN_ID на b24_field=COMPANY_ID учитывая предыдущую замену при импорте демоданных
@@ -33,25 +35,33 @@ def make_links_from_origin(data, excel_field, b24_field, source_map, prefix):
     for d in data:
         # Добавляем префикс для ORIGIN_ID
         if d.get(excel_field):
-            d[b24_field] = source_map["{}_{}".format(prefix, d.get(excel_field))]['ID']
+            d[b24_field] = \
+                source_map["{}_{}".format(prefix, d.get(excel_field))]['ID']
     return data
+
 
 def import_data_from_xls(filename, but):
     # Принимает токен и excel файл с несколькоми страницами и загружает их в Битрикс24
-    #sheet_names = get_sheet_names(filename)
+    # sheet_names = get_sheet_names(filename)
     excel_file = pd.ExcelFile(filename)
     # для всего пакета загрузки будет одинаковый префикс у ORIGIN_ID
     load_origin_id_prefix = time.time()
+    object_count = {"Лиды": None,
+                    "Сделки": None,
+                    "Контакты": None,
+                    "Компании": None}
 
-    if "Загружаем компании": #Всегда True
+    if "Загружаем компании":  # Всегда True
         company_data = excel_file.parse('Компании').to_dict("records")
         company_data = add_origin_prefix(company_data, load_origin_id_prefix)
+        object_count["Компании"] = len(company_data)
         methods = []
         for company in company_data:
             # Создаем массив методов которые нам нужно выполнить в Битрикс24
             # делаем просто 1 вызов = 1 компания
             methods.append(
-                ('crm.item.batchImport', {"entityTypeId": '4', "data": [company]}))
+                ('crm.item.batchImport',
+                 {"entityTypeId": '4', "data": [company]}))
         # Теперь когда мы набрали список методов, то можно их выполнить с помощью batch_api_call
         but.batch_api_call(methods)
 
@@ -60,7 +70,8 @@ def import_data_from_xls(filename, but):
             "FILTER": {"%ORIGIN_ID": "{}_".format(load_origin_id_prefix)}})
         # Используем прием конвертации в адресный dict https://it-solution.kdb24.ru/article/218199/
         # после этого легко найдем companies_origin_id_dict['1690182208.5614886_7']['ID']
-        companies_origin_id_dict = {item['ORIGIN_ID']: item for item in companies}
+        companies_origin_id_dict = {item['ORIGIN_ID']: item for item in
+                                    companies}
         for d in company_data:
             # https://dev.1c-bitrix.ru/rest_help/crm/requisite/methods/crm_address_add.php
             but.call_api_method("crm.address.add", {"fields": {
@@ -74,11 +85,44 @@ def import_data_from_xls(filename, but):
     if "Загружаем контакты":
         contacts_data = excel_file.parse('Контакты').to_dict("records")
         contacts_data = add_origin_prefix(contacts_data, load_origin_id_prefix)
-        contacts_data = make_links_from_origin(contacts_data, 'COMPANY_ORIGIN_ID', 'COMPANY_ID', companies_origin_id_dict, load_origin_id_prefix)
-        but.call_api_method("crm.item.batchImport", {"entityTypeId": '3', "data": contacts_data})
+        contacts_data = make_links_from_origin(contacts_data,
+                                               'COMPANY_ORIGIN_ID',
+                                               'COMPANY_ID',
+                                               companies_origin_id_dict,
+                                               load_origin_id_prefix)
+        object_count["Контакты"] = len(contacts_data)
+        methods = []
+        for contact in contacts_data:
+            methods.append(
+                ('crm.item.batchImport',
+                 {"entityTypeId": '3', "data": [contact]}))
+        but.batch_api_call(methods)
+        # but.call_api_method("crm.item.batchImport",
+        #                     {"entityTypeId": '3', "data": contacts_data})
 
+    if "Загружаем сделки":
+        deals_data = excel_file.parse('Сделки').to_dict("records")
+        object_count["Сделки"] = len(deals_data)
+        methods = []
+        for deal in deals_data:
+            methods.append(
+                ('crm.item.batchImport',
+                 {"entityTypeId": '2', "data": [deal]}))
+        but.batch_api_call(methods)
+        # but.call_api_method("crm.item.batchImport",
+        #                     {"entityTypeId": '2', "data": deal_data})
+    if "Загружаем лиды":
+        leads_data = excel_file.parse('Лиды').to_dict("records")
+        object_count["Лиды"] = len(leads_data)
+        methods = []
+        for lead in leads_data:
+            methods.append(
+                ('crm.item.batchImport',
+                 {"entityTypeId": '1', "data": [lead]}))
+        but.batch_api_call(methods)
 
-    return
+    return object_count
+
 
 def excel_to_dict(file_path, sheet_name):
     workbook = load_workbook(filename=file_path)
@@ -94,9 +138,11 @@ def excel_to_dict(file_path, sheet_name):
     for row in sheet.iter_rows(min_row=2, values_only=True):
         row_data = dict(zip(headers, row))
         if sheet_name == "Контакты":
-            row_data["PHONE"] = [{"VALUE": str(row_data["PHONE"]), "VALUE_TYPE": "WORK"}]
+            row_data["PHONE"] = [
+                {"VALUE": str(row_data["PHONE"]), "VALUE_TYPE": "WORK"}]
         elif sheet_name == "Компании":
-            address_company.append({"ADDRESS_1": str(row_data["ADDRESS"]), "CITY": str(row_data["ADDRESS_CITY"])})
+            address_company.append({"ADDRESS_1": str(row_data["ADDRESS"]),
+                                    "CITY": str(row_data["ADDRESS_CITY"])})
             origin_dict[origin_id] = None
         data.append(row_data)
         origin_id += 1
