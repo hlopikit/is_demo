57ee58986b7a3837de171174ba8a2f1ee29d9f76
kiv-itsolution <kiv@it-solution.ru>
2023-08-02 17:05:56 +0300

    open ai bot ready for merge

--- tg_openai_bot/views/start_page.py
+++ tg_openai_bot/views/start_page.py
@@ -10,5 +10,6 @@ def start_page_open_ai(request):
         # Получаем токен бота и чатайди из формы юзера
         bot_token = request.POST.get('bot_token')
         chat_id = request.POST.get('chat_id')
+        # Подгружаем бота в чат
         say_hello(register_bot(bot_token), chat_id)
     return render(request, 'open_ai_start.html')
