fdc6c1efc139edeaf1538c62bfc1a1734a6816a3
kiv-itsolution <kiv@it-solution.ru>
2023-08-04 14:40:47 +0300

    js added, app done

--- tg_openai_bot/views/start_page.py
+++ tg_openai_bot/views/start_page.py
@@ -1,12 +1,19 @@
+import threading
+
 from django.shortcuts import render
 
 from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
 from tg_openai_bot.cron import handle_bot_updates
 
 
+def run_bot_thread():
+    while True:  # Needed cron func, while only in prod stage
+        handle_bot_updates('tg_openai_bot.models.open_ai_bot.OpenAiBot')
+
+
 @main_auth(on_cookies=True)
 def start_page_open_ai(request):
     if request.method == 'POST':
-        while True:  # Needed cron func, while only in prod stage
-            handle_bot_updates('tg_openai_bot.models.open_ai_bot.OpenAiBot')
+        foo_loop = threading.Thread(target=run_bot_thread(), daemon=True)
+        foo_loop.start()
     return render(request, 'open_ai_start.html')
