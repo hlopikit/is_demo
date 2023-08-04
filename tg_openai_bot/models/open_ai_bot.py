254b4db853eb4a11de3cf7675b8276c39dd099c1
kiv-itsolution <kiv@it-solution.ru>
2023-08-04 13:16:05 +0300

    added second form

--- tg_openai_bot/models/open_ai_bot.py
+++ tg_openai_bot/models/open_ai_bot.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from intagration_utils_candidate.app_telegram_bot.models.abstract_bot import AbstractBot
 from tg_openai_bot.models import OpenAiUser, OpenAiUserChat, OpenAiUserMessage
 
+# Импорт библиотеки OpenAI
 import openai
 openai.api_key = settings.OPEN_AI_API_KEY
 
@@ -13,16 +14,18 @@ def handle_updates():
     OpenAiBot.objects.get(pk=1).handle_updates()
 
 
+# Бот на основе AbstractBot
 class OpenAiBot(AbstractBot):
     USER_CLASS = OpenAiUser
     CHAT_CLASS = OpenAiUserChat
     MESSAGE_CLASS = OpenAiUserMessage
-    CONTEXT = []
-    VALID_COMMAND = ['/start', '/gpt']
+    CONTEXT = []  # Константа для будущего сохранения диалога
 
+    # Хендлер проверяющей команды старб
     def on_start_command(self, message, t_user, t_chat, param):
         self.send_message(t_chat.telegram_id, "Команда start принята, чтобы спросить бота, напишите /gpt")
 
+    #  Хендлер команды для запроса к GPT, используем модель gpt-3.5-turbo
     def on_gpt_command(self, message, t_user, t_chat, param):
         if not param:
             self.send_message(t_chat.telegram_id, 'Введите запрос')
@@ -36,8 +39,23 @@ class OpenAiBot(AbstractBot):
         self.send_message(t_chat.telegram_id, response_content)
         OpenAiBot.CONTEXT.append({'role': 'assistant', 'content': response_content})
 
+    # Хендлер команды помощь
     def on_help_command(self, message, t_user, t_chat, param):
         self.send_message(t_chat.telegram_id, "На данный момент доступны следующие команды:\n"
                                               "/start - проверка бота\n"
                                               "/gpt - задать вопрос")
 
+    # Проверка на существующую команду
+    def check_command(self, message, t_user, t_chat):
+        if message.text.startswith('/'):
+            # Проверяем, является ли сообщение командой
+            command = message.text.split(' ')[0]
+            if command == '/start':
+                self.on_start_command(message, t_user, t_chat, param=None)
+            elif command == '/gpt':
+                self.on_gpt_command(message, t_user, t_chat, param=message.text[len(command) + 1:])
+            elif command == '/help':
+                self.on_help_command(message, t_user, t_chat, param=None)
+            else:
+                # Если команда не распознана, можно отправить ответ с сообщением об ошибке
+                self.send_message(t_chat.telegram_id, "Неизвестная команда. Для списка доступных команд введите /help")
--- tg_openai_bot/views/start_page.py
+++ tg_openai_bot/views/start_page.py
@@ -1,9 +1,5 @@
-import time
-
 from django.shortcuts import render
 
-
-# from tg_openai_bot.utils.utils import say_hello, register_bot
 from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
 from tg_openai_bot.cron import handle_bot_updates
 
@@ -11,12 +7,15 @@ from tg_openai_bot.cron import handle_bot_updates
 @main_auth(on_cookies=True)
 def start_page_open_ai(request):
     if request.method == 'POST':
-        # Получаем токен бота и чатайди из формы юзера
-        bot_token = request.POST.get('bot_token')
-        chat_id = request.POST.get('chat_id')
+
+        # Получаем токен бота и чатайди из формы юзера, для stand alone бота ниже
+        # bot_token = request.POST.get('bot_token')
+        # chat_id = request.POST.get('chat_id')
+
         while True:
             handle_bot_updates('tg_openai_bot.models.open_ai_bot.OpenAiBot')
-        # Подгружаем старого бота в чат
+
+        # Подгружаем stand alone бота в чат с приветственным сообщением
         # say_hello(register_bot(bot_token), chat_id)
 
     return render(request, 'open_ai_start.html')
