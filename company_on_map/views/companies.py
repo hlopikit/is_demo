638275dbf253210057dc4910523585e1ae1ba4e9
akapu <aak@it-solution.ru>
2023-08-14 11:39:17 +0300

    теперь вью companies возвращает json со словарем со всеми компаниями с указанным адресом

--- company_on_map/views/companies.py
+++ company_on_map/views/companies.py
@@ -9,6 +9,20 @@ def companies(request):
     all_companies = but.call_list_method("crm.company.list", {
         "select": ["ID", "TITLE"]
     })
-    all_companies = {c["ID"]: c for c in all_companies.items()}
+    all_companies = {c["ID"]: c for c in all_companies}
+
+    all_addresses = but.call_list_method("crm.address.list", {
+        "order": {"TYPE_ID": "ASC"},
+        "select": ["ADDRESS_1", "PROVINCE", "COUNTRY", "ANCHOR_ID"],
+        "filter": {
+            "ANCHOR_TYPE_ID": "4"
+        }
+    })
+
+    comps_w_addr = {}
+    for a in all_addresses:
+        comp = comps_w_addr.setdefault(a["ANCHOR_ID"], {})
+        comp.setdefault("addr", []).append(a)
+        comp["title"] = all_companies[a["ANCHOR_ID"]]["TITLE"]
 
     return JsonResponse(all_companies)
