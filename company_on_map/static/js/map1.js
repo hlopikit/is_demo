6f765fad1840f4a0618af0a5476e5a503d84d1ad
akapu <aak@it-solution.ru>
2023-08-14 12:35:44 +0300

    скрипт жс теперь запрашивает у нашего приложения список компаний

--- company_on_map/static/js/map1.js
+++ company_on_map/static/js/map1.js
@@ -7,18 +7,22 @@ function init(){
             searchControlProvider: 'yandex#search'
             });
 
-    BX24.callMethod('crm.company.list',{select:['TITLE','ID','ADDRESS_CITY','ADDRESS']}, function (comp) {
-        let Companies = comp.answer.result
-        Companies.forEach((company) => {
-            let object = ymaps.geocode(`${company['ADDRESS_CITY']}, ${company['ADDRESS']}`)
-            object.then(
-            function (res) {
-                let coor = res.geoObjects.properties._data.metaDataProperty.GeocoderResponseMetaData.Point.coordinates
-                myMap.geoObjects.add(new ymaps.Placemark([coor[1], coor[0]], {balloonContent: `<strong>${company['TITLE']}</strong>` + '\n' + `${company['ADDRESS_CITY']}, ${company['ADDRESS']}`}, ));
-            })
-        })
-        if (comp.more())
-            comp.next()
+    let req = new XMLHttpRequest();
 
-    })
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let companies = JSON.parse(this.responseText);
+
+            for (const comp of Object.values(companies)) {
+                let object = ymaps.geocode(`${comp['addr'][0]['PROVINCE']}, ${comp['addr'][0]['ADDRESS_1']}`)
+                object.then(function (res) {
+                    let coor = res.geoObjects.properties._data.metaDataProperty.GeocoderResponseMetaData.Point.coordinates
+                    myMap.geoObjects.add(new ymaps.Placemark([coor[1], coor[0]], {balloonContent: `<strong>${comp['title']}</strong>` + '\n' + `${comp['addr'][0]['PROVINCE']}, ${comp['addr'][0]['ADDRESS_1']}`}, ));
+                })
+            }
+        }
+    };
+
+    req.open("GET", "/company_on_map/companies");
+    req.send();
 }
