eab818239eba89cfc10fffcc13cb3325e25d3224
akapu <aak@it-solution.ru>
2023-08-17 11:40:34 +0300

    use batch_api_call now

--- import_company_google/views/import_company_view.py
+++ import_company_google/views/import_company_view.py
@@ -21,14 +21,17 @@ def import_company_google(request):
 
             but = request.bitrix_user_token
 
+            methods = []
             batch_size = 20
             batch = []
             for company in data:
                 batch.append(company)
                 if len(batch) % batch_size == 0:
-                    but.call_api_method("crm.item.batchImport", {"entityTypeId": 4, "data": batch})
+                    methods.append(("crm.item.batchImport", {"entityTypeId": 4, "data": batch}))
                     batch = []
-            but.call_api_method("crm.item.batchImport", {"entityTypeId": 4, "data": batch})
+            methods.append(("crm.item.batchImport", {"entityTypeId": 4, "data": batch}))
+
+            but.batch_api_call(methods)
 
             result = "Компании успешно добавлены!"
         except Exception as Ex:
