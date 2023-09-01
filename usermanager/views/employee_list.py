a13ef39313721a23ba24eef3856dec1a85dbbce2
akapu <aak@it-solution.ru>
2023-09-01 11:07:54 +0300

    fix hardcoded in template portal names

--- usermanager/views/employee_list.py
+++ usermanager/views/employee_list.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from ..utils.search_manager import search_manager
 from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
+from django.conf import settings
 
 
 @main_auth(on_cookies=True)
@@ -8,4 +9,7 @@ def employee_list(request):
     but = request.bitrix_user_token
     user_dict, user_fields = search_manager(but)
 
-    return render(request, 'list.html', context={'fields': user_fields, 'users': user_dict})
+    return render(request, 'list.html', {
+        'portal': settings.APP_SETTINGS.portal_domain,
+        'users': user_dict
+    })
